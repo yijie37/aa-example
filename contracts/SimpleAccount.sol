6c0ba43adc325d00b3ab146f765e6326bcc5dd92
liyijie1 <liyijie1@360.cn>
2023-03-21 15:17:32 +0800

    fix: 1.check root; 2.check owner; 3.add poseidon hash; 4.fix nullifier unset

--- contracts/SimpleAccount.sol
+++ contracts/SimpleAccount.sol
@@ -152,17 +152,25 @@ contract SimpleAccount is BaseAccount, UUPSUpgradeable, Initializable {
         _onlyOwner();
     }
 
+    function getGuardians() external view returns (uint[] memory) {
+        GuardianStorage.layout().getGuardians();
+    }
+
     function initilizeGuardians(
       uint[] memory guardians,
       uint vote_threshold,
-      IUpdateGuardianVerifier updateGuardianVerifier, 
-      ISocialRecoveryVerifier socialRecoveryVerifier
-    ) external {
+      uint root,
+      address updateGuardianVerifierAddress,
+      address socialRecoveryVerifierAddress,
+      address poseidonContractAddress
+    ) external onlyOwner {
       GuardianStorage.layout().initialize(
         guardians,
         vote_threshold,
-        updateGuardianVerifier, 
-        socialRecoveryVerifier
+        root,
+        updateGuardianVerifierAddress, 
+        socialRecoveryVerifierAddress ,
+        poseidonContractAddress
       );
     }
 
@@ -182,7 +190,7 @@ contract SimpleAccount is BaseAccount, UUPSUpgradeable, Initializable {
       uint[2] memory c, 
       uint[3] memory input 
     ) external returns (bool valid, bool update) {
-      (valid, update) = GuardianStorage.layout().recover(a, b, c, input);
+      (valid, update) = GuardianStorage.layout().recover(a, b, c, input, newOwner);
       if(valid && update) {
         owner = newOwner;
       }
